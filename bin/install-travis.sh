a766a5c0d983915f96ac797b24dee9c641041107
PedroAriasRuiz <asiganthesunk@gmail.com>
2019-11-04 13:14:00 +0100

    Remove duplicated line
    removed duplicated cannonical-weth entry in install-travis.sh

--- bin/install-travis.sh
+++ bin/install-travis.sh
@@ -4,7 +4,6 @@ npm install
 npm install -g ganache-cli@6.6.0
 npm install -g truffle@5.0.38
 npm̀ install openzeppeling-solidity
-npm install cannonical-weth
 npm install @openzeppelin/test-helpers
 npm install web3
 npm install --save truffle-contract canonical-weth
